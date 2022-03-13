df475738e8ca44abadbbdb9cb262cd6082ddec9b
Stefan Probst <stefanprobst@posteo.de>
2022-03-13 10:05:02 +0100

    fix: add collator

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -4,15 +4,15 @@ import { render, screen } from '@testing-library/react'
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 import type { NextRouter } from 'next/router'
 import type { FC, ReactNode } from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react'
 
 import { I18nProvider, useI18n } from '../src'
 
-const locale = 'en'
+const defaultLocale = 'en'
 
-function createWrapper(dict: object): FC {
-  const mockRouter = {
-    locale,
-  } as NextRouter
+function createWrapper(dict: object, locale = defaultLocale): FC {
+  const mockRouter = { locale } as NextRouter
 
   return function Wrapper(props) {
     return (
@@ -27,7 +27,7 @@ it('retrieves translation by string key', () => {
   const dict = { common: { hello: 'Hello' } }
 
   function App() {
-    const { t } = useI18n<typeof dict, typeof locale>()
+    const { t } = useI18n<typeof dict, typeof defaultLocale>()
 
     return <h1>{t('common.hello')}</h1>
   }
@@ -41,7 +41,7 @@ it('retrieves translation by array key', () => {
   const dict = { common: { hello: 'Hello' } }
 
   function App() {
-    const { t } = useI18n<typeof dict, typeof locale>()
+    const { t } = useI18n<typeof dict, typeof defaultLocale>()
 
     return <h1>{t(['common', 'hello'])}</h1>
   }
@@ -55,7 +55,7 @@ it('substitutes value', () => {
   const dict = { common: { hello: 'Hello {{name}}' } }
 
   function App() {
-    const { t } = useI18n<typeof dict, typeof locale>()
+    const { t } = useI18n<typeof dict, typeof defaultLocale>()
 
     return <h1>{t(['common', 'hello'], { values: { name: 'Stefan' } })}</h1>
   }
@@ -80,7 +80,7 @@ it('uses plural', () => {
     },
   }
   function App() {
-    const { t, plural } = useI18n<typeof dict, typeof locale>()
+    const { t, plural } = useI18n<typeof dict, typeof defaultLocale>()
 
     return (
       <main>
@@ -104,7 +104,7 @@ it('substitutes component', () => {
   const dict = { common: { hello: 'Hello <s>{{name}}</s>' } }
 
   function App() {
-    const { t } = useI18n<typeof dict, typeof locale>()
+    const { t } = useI18n<typeof dict, typeof defaultLocale>()
 
     return (
       <h1>{t(['common', 'hello'], { values: { name: 'Stefan' }, components: { s: 'button' } })}</h1>
@@ -126,7 +126,7 @@ it('substitutes JSX component', () => {
   }
 
   function App() {
-    const { t } = useI18n<typeof dict, typeof locale>()
+    const { t } = useI18n<typeof dict, typeof defaultLocale>()
 
     return (
       <h1>{t(['common', 'hello'], { values: { name: 'Stefan' }, components: { s: Button } })}</h1>
@@ -138,4 +138,153 @@ it('substitutes JSX component', () => {
   expect(screen.getByRole('heading')).toHaveTextContent('Hello Stefan')
   expect(screen.getByRole('button')).toHaveTextContent('Stefan')
   expect(screen.getByRole('button')).toHaveClass('primary')
+})
+
+it('sorts array', () => {
+  const dict = {}
+
+  function App() {
+    const { sort } = useI18n<typeof dict, typeof defaultLocale>()
+    const list = ['z', 'a', 'ä']
+
+    return (
+      <ul>
+        {sort(list).map((item) => {
+          return <li key={item}>{item}</li>
+        })}
+      </ul>
+    )
+  }
+
+  render(<App />, { wrapper: createWrapper(dict) })
+
+  expect(screen.getByRole('list')).toMatchInlineSnapshot(`
+    <ul>
+      <li>
+        a
+      </li>
+      <li>
+        ä
+      </li>
+      <li>
+        z
+      </li>
+    </ul>
+  `)
+})
+
+it('sorts array according to non-default language', () => {
+  const dict = {}
+
+  function App() {
+    const { sort } = useI18n<typeof dict, typeof defaultLocale>()
+    const list = ['z', 'a', 'ä']
+
+    return (
+      <ul>
+        {sort(list).map((item) => {
+          return <li key={item}>{item}</li>
+        })}
+      </ul>
+    )
+  }
+
+  render(<App />, { wrapper: createWrapper(dict, 'sv') })
+
+  expect(screen.getByRole('list')).toMatchInlineSnapshot(`
+    <ul>
+      <li>
+        a
+      </li>
+      <li>
+        z
+      </li>
+      <li>
+        ä
+      </li>
+    </ul>
+  `)
+})
+
+it('sorts array of objects', () => {
+  const dict = {}
+
+  function App() {
+    const { createCollator } = useI18n<typeof dict, typeof defaultLocale>()
+    const compare = createCollator()
+    const list = [
+      { id: 1, label: 'z' },
+      { id: 2, label: 'a' },
+      { id: 3, label: 'ä' },
+    ]
+
+    return (
+      <ul>
+        {list
+          .sort((a, b) => {
+            return compare(a.label, b.label)
+          })
+          .map((item) => {
+            return <li key={item.id}>{item.label}</li>
+          })}
+      </ul>
+    )
+  }
+
+  render(<App />, { wrapper: createWrapper(dict) })
+
+  expect(screen.getByRole('list')).toMatchInlineSnapshot(`
+    <ul>
+      <li>
+        a
+      </li>
+      <li>
+        ä
+      </li>
+      <li>
+        z
+      </li>
+    </ul>
+  `)
+})
+it('sorts array of objects according to non-default language', () => {
+  const dict = {}
+
+  function App() {
+    const { createCollator } = useI18n<typeof dict, typeof defaultLocale>()
+    const compare = createCollator()
+    const list = [
+      { id: 1, label: 'z' },
+      { id: 2, label: 'a' },
+      { id: 3, label: 'ä' },
+    ]
+
+    return (
+      <ul>
+        {list
+          .sort((a, b) => {
+            return compare(a.label, b.label)
+          })
+          .map((item) => {
+            return <li key={item.id}>{item.label}</li>
+          })}
+      </ul>
+    )
+  }
+
+  render(<App />, { wrapper: createWrapper(dict, 'sv') })
+
+  expect(screen.getByRole('list')).toMatchInlineSnapshot(`
+    <ul>
+      <li>
+        a
+      </li>
+      <li>
+        z
+      </li>
+      <li>
+        ä
+      </li>
+    </ul>
+  `)
 })
